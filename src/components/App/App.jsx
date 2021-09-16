73e69ea9f62bfc56a0f7c435ee4d119b31b6dad2
Tou Xiong <tlxiong.prime@outlook.com>
2021-09-16 11:36:05 -0500

    created GET for app.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,30 +1,46 @@
-import React from 'react';
-import Header from '../Header/Header.jsx'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
+import Header from '../Header/Header.jsx';
 import './App.css';
 import InputItemForm from '../InputItemForm/InputItemForm.jsx';
 import ResetAndClearButton from '../ResetAndClearButton/ResetAndClearButton.jsx';
 import ItemList from '../ItemList/ItemList.jsx';
 import CompletedItemList from '../CompletedItemList/CompletedItemList.jsx';
 
-//Any variables and functions needed by Greg can go here:
 
-
-
-//Any variables and functions needed by Tou can go here:
-
-
-
-//Any variables and functions needed by Anwar can go here:
-
-
-
-//Any variables and functions needed by Andy can go here:
-
-
-
-
-//Only edit the component you're working on below if props are needed
 function App() {
+    let [itemList, setList] = useState([]);
+
+    //Any variables and functions needed by Greg can go here:
+    
+    
+    
+    //Any variables and functions needed by Tou can go here:
+    useEffect(() => {
+        getList()
+    
+    }, [])
+    
+    const getList = () => {
+        axios.get('/list').then(response => {
+            setList(response.data)
+        }).catch((error) => {
+            console.log('error in GET', error);
+        })
+    }
+    
+    //Any variables and functions needed by Anwar can go here:
+    
+    
+    
+    //Any variables and functions needed by Andy can go here:
+    
+    
+    
+    
+    //Only edit the component you're working on below if props are needed
+
     return (
         <div className="App">
             <Header />
