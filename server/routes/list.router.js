acf647076c7106ffb4c5ce344ef58d60620e669c
anwar <anwarhussein2353@gmail.com>
2021-09-16 13:49:46 -0500

    uncommented stuff

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -2,57 +2,57 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool.js');
 
-// TODO - Add routes here...
+//TODO - Add routes here...
 
 
-// GET list items from database
-// router.get('/', (req, res) => {
-//     console.log('Getting items from list');
+//GET list items from database
+router.get('/', (req, res) => {
+    console.log('Getting items from list');
     
-//     const queryText = 'SELECT * FROM "tasks" ORDER BY "item";';
-// }).then((response) => {
-//     console.log('');
+    const queryText = 'SELECT * FROM "tasks" ORDER BY "item";';
+}).then((response) => {
+    console.log('');
     
-// }).catch((error) => {
-//     console.log(error);
-//     alert();
-// });
+}).catch((error) => {
+    console.log(error);
+    alert();
+});
 
 
-//  POST new item 
-// router.post('/', (req, res) => {
-//     console.log('Posting items to list', req.body);
+ //POST new item 
+router.post('/', (req, res) => {
+    console.log('Posting items to list', req.body);
     
-// }).then((response) => {
+}).then((response) => {
 
-// }).catch((error) => {
-//     console.log(error);
-//     alert();
-// });
+}).catch((error) => {
+    console.log(error);
+    alert();
+});
 
 
-// PUT ('/:id')
-// router.put('/:id', (req, res) => {
-//     console.log('PUT request received');
+//PUT ('/:id')
+router.put('/:id', (req, res) => {
+    console.log('PUT request received');
     
-// }).then((response) => {
+}).then((response) => {
 
-// }).catch((error) => {
-//     console.log(error);
-//     alert();
-// });
+}).catch((error) => {
+    console.log(error);
+    alert();
+});
 
 
-// DELETE ('/:id')
-// router.delete('/:id', (req, res) => {
-//     console.log('DELETE request received');
+//DELETE ('/:id')
+router.delete('/:id', (req, res) => {
+    console.log('DELETE request received');
     
-// }).then((response) => {
+}).then((response) => {
 
-// }).catch((error) => {
-//     console.log(error);
-//     alert();
-// });
+}).catch((error) => {
+    console.log(error);
+    alert();
+});
 
 
 
