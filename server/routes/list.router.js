0bda0231689fc9cfd709ee5688def496d73624f2
Greg Tambornino <gregtambornino@gmail.com>
2021-09-16 09:46:32 -0500

    initial empty server routes added

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -5,4 +5,45 @@ const pool = require('../modules/pool.js');
 // TODO - Add routes here...
 
 
+// GET list items from database
+router.get('/' (req, res) => {
+
+}).then((response) => {
+
+}).catch((error) => {
+
+});
+
+
+//  POST new item 
+router.post('/' (req, res) => {
+
+}).then((response) => {
+
+}).catch((error) => {
+
+});
+
+
+// PUT ('/:id')
+router.put('/:id' (req, res) => {
+
+}).then((response) => {
+
+}).catch((error) => {
+
+});
+
+
+// DELETE ('/:id')
+router.delete('/:id' (req, res) => {
+
+}).then((response) => {
+
+}).catch((error) => {
+
+});
+
+
+
 module.exports = router;
