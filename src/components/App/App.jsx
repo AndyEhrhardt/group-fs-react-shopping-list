cbaba738c39352b4eb9a4dc4c65559174f0630e6
AndrewEhrhardt <andyehrhardt@gmail.com>
2021-09-16 09:49:21 -0500

    app is organized

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,35 @@
 import React from 'react';
-
 import Header from '../Header/Header.jsx'
 import './App.css';
+import InputItemForm from '../InputItemForm/InputItemForm.jsx'
+import ResetAndClearButton from '../ResetAndClearButton/ResetAndClearButton.jsx'
+import ItemList from '../ItemList/ItemList.jsx'
+import CompletedItemList from '../CompletedItemList/CompletedItemList.jsx'
+
+//Any variables and functions needed by Greg can go here:
+
+
+
+//Any variables and functions needed by Tou can go here:
+
+
+
+//Any variables and functions needed by Anwar can go here:
+
+
+
+//Any variables and functions needed by Andy can go here:
+
 
 
 function App() {
     return (
         <div className="App">
             <Header />
-            <main>
-                <p>Under Construction...</p>
-            </main>
+            <InputItemForm />
+            <ResetAndClearButton />
+            <ItemList />
+            <CompletedItemList />
         </div>
     );
 }
