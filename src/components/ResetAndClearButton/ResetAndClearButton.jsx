a3514e45dfb2b7245fd589982553de678840268b
Greg Tambo <gregtambornino@gmail.com>
2021-09-16 11:50:40 -0500

    initial ideas for clear and reset buttons

--- src/components/ResetAndClearButton/ResetAndClearButton.jsx
+++ src/components/ResetAndClearButton/ResetAndClearButton.jsx
@@ -1,10 +1,25 @@
 
+function resetAllItems ({itemList}) {
+    // clear purchased status
+    // might need to call the PUT request here doing something like this:
+    for (item of itemList) {
+        item.completed = !completed;
+        }
+    props.getList();
+}
+
+function clearAllItems(event) {
+    // might have to call DELETE request here
+    props.getList();
+}
+
+
 function changeListItems(props) {
     console.log('Resetting items ');
     return (
         <>
-            <button onClick={/* ??? */}>Reset</button>
-            <button onClick={/* ??? */}>Delete</button>
+            <button onClick={/* entire list ??? */}>Reset</button>
+            <button onClick={/* entire list ??? */}>Clear</button>
         </>
     )    
 }
